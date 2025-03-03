19f5ae1675e48a76021c50f1f8ccebcb3593a950
Yazi27 <yannbricage@pm.me>
2025-03-03 22:46:20 +0100

    Used stable renderers with keys to avoid rerenders
    - Since react did not know what the tree would look like with conditional renders, we had to keep the props stable'

--- src/components/Sailor.tsx
+++ src/components/Sailor.tsx
@@ -9,23 +9,39 @@ interface SailorProps {
   children: React.ReactNode;
 }
 
+function StableRenderer({
+  isSailing,
+  fallback,
+  children,
+}: {
+  isSailing: boolean;
+  fallback: React.ReactNode;
+  children: React.ReactNode;
+}) {
+  return (
+    <div>
+      {isSailing ? (
+        <div key="fallback">{fallback}</div>
+      ) : (
+        <div key="children">{children}</div>
+      )}
+    </div>
+  );
+}
+
 export default function Sailor({ fallback, children }: SailorProps) {
   const [isSailing, setIsSailing] = useState(getVoyageStatus());
 
-  console.log("rendering");
-
   useEffect(() => {
     const unsubscribe = board((sailing: boolean) => {
-      console.log("Calling callback", sailing);
       setIsSailing(sailing);
     });
     return () => unsubscribe();
   }, []);
 
   return (
-    <>
-      <div style={{ display: isSailing ? "none" : "block" }}>{children}</div>
-      {isSailing && fallback}
-    </>
+    <StableRenderer isSailing={isSailing} fallback={fallback}>
+      {children}
+    </StableRenderer>
   );
 }
